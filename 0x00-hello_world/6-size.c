74a20349a0cddf82be929a305c35118761561add
Humanerror <37669957+KudzanaiGomera@users.noreply.github.com>
2022-03-11 01:05:10 +0200

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,7 +11,7 @@ int main()
 	int intType;
 	float floatType;
 	long long int  doubleType;
-	long int longType
+	long int longType;
 	char charType;
 
 	/* sizeof evaluates the size of a variable */
