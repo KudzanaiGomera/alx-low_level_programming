063fc9c83886743c6ee620e9548278491424ddf7
KudzanaiGomera <kgomera@student.wethinkcode.co.za>
2022-03-11 00:20:16 +0200

    init 3

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	/* print a string to stdout */
+	puts("Programming is like building a multilingual puzzle\n");
+
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	/* print a string to stdout */
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
