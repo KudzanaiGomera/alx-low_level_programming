33aba91d8e330681eeb0160366f5fcfd6c044a75
Humanerror <37669957+KudzanaiGomera@users.noreply.github.com>
2022-03-11 01:38:10 +0200

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,6 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 58);
-	printf("\n");
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 30);
 	return (1);
 }
