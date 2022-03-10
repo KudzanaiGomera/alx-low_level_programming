bf27444e991da2492644f8a581e56349f3ebc9ac
Humanerror <37669957+KudzanaiGomera@users.noreply.github.com>
2022-03-11 01:06:34 +0200

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,11 +15,11 @@ int main()
 	char charType;
 
 	/* sizeof evaluates the size of a variable */
-	printf("Size of char: %zu byte(s)\n", sizeof(charType));
+	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
 	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
-	printf("Size of long int: %zu byte(s)\n", sizeof(longType));
-	printf("Size of long long int: %zu byte(s)\n", sizeof(doubleType));
-	printf("Size of float: %zu byte(s)\n", sizeof(floatType));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(longType));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(doubleType));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
     
     return (0);
 }
