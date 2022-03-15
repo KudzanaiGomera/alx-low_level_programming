21a13030746c309ee076609ceb456e533347f424
KudzanaiGomera <kgomera@student.wethinkcode.co.za>
2022-03-16 00:17:53 +0200

    almost done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *prints_alphabet fnc that prints alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+    printf("abcdefghijklmnopqrstuvwxyz\n");
+}
