43a700fd421dbdb4b4ff7497b4373420cb20bc71
Humanerror <37669957+KudzanaiGomera@users.noreply.github.com>
2022-03-28 13:19:25 +0200

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,9 +2,9 @@
 
 /**
  * _memset - a function that fills memory with constant bytes
- * @b: location to fill
- * @c: char to fill location with
- * @len: number of bytes to fill
+ * @s: location to fill
+ * @b: char to fill location with
+ * @n: number of bytes to fill
  * Return: returns pointer to location filled
  */
 char *_memset(char *s, char b, unsigned int n)
